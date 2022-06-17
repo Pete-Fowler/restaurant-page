80157e332b7f23bdaadd192f97e53a80ca0a7d59
Pete Fowler <fowler.pete@gmail.com>
2022-06-17 16:02:34 -0600

    Use bigger image for background, begin header

--- src/home.js
+++ src/home.js
@@ -1,20 +1,22 @@
-import bowl from './phoBowl.jpg';
+// import bowl from './assets/phoBowl.jpg';
+import './style.css';
 
 export default function home() {
     const element = document.createElement('div');
+    element.id = 'home';
     
     const h1 = document.createElement('h1');
-    h1.textContent = `Pho Real`;
+    h1.textContent = `Is This Pho Real?`;
     element.appendChild(h1);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Best Vietnamese food ever!';
     element.appendChild(h2);
 
-    const phoBowl = document.createElement('img');
-    phoBowl.src = bowl;
-    phoBowl.setAttribute('style', 'width: 400px');
-    element.appendChild(phoBowl);
+    // const phoBowl = document.createElement('img');
+    // phoBowl.id = 'bowl';
+    // phoBowl.src = bowl;
+    // element.appendChild(phoBowl);
 
     return element;
 }
