4f6267d2bcf8a8e6d03beab78c1f33055358ffee
Pete Fowler <fowler.pete@gmail.com>
2022-06-16 20:13:10 -0600

    Add home image

--- src/home.js
+++ src/home.js
@@ -1,10 +1,20 @@
+import bowl from './phoBowl.jpg';
 
 export default function home() {
     const element = document.createElement('div');
     
-    const headline = document.createElement('h1');
-    headline.textContent = 'Pho Real -- Best Vietnamese food _ever_';
-    element.appendChild(headline);
+    const h1 = document.createElement('h1');
+    h1.textContent = `Pho Real`;
+    element.appendChild(h1);
+
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Best Vietnamese food ever!';
+    element.appendChild(h2);
+
+    const phoBowl = document.createElement('img');
+    phoBowl.src = bowl;
+    phoBowl.setAttribute('style', 'width: 400px');
+    element.appendChild(phoBowl);
 
     return element;
 }
