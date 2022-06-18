3e517b1fab8ee7489cdcbebda66dd1f992c89552
Pete Fowler <fowler.pete@gmail.com>
2022-06-18 16:49:44 -0600

    Add about paragraph

--- src/assets/modules/header.js
+++ src/assets/modules/header.js
@@ -10,24 +10,6 @@ el.appendChild(h1);
 const btns = document.createElement('div');
 btns.id = 'btn-group';
 
-// const homeBtn = document.createElement('button');
-// homeBtn.classList.add('btn');
-// homeBtn.id = 'home-btn';
-// homeBtn.textContent = 'Home';
-// btns.appendChild(homeBtn);
-
-// const menuBtn = document.createElement('button');
-// menuBtn.classList.add('btn');
-// menuBtn.id = 'menu-btn';
-// menuBtn.textContent = 'Menu';
-// btns.appendChild(menuBtn);
-
-// const contactBtn = document.createElement('button');
-// contactBtn.classList.add('btn');
-// contactBtn.id = 'contact-btn';
-// contactBtn.textContent = 'Contact';
-// btns.appendChild(contactBtn);
-
 btns.appendChild(makeBtn('Home'));
 btns.appendChild(makeBtn('Menu'));
 btns.appendChild(makeBtn('Contact'));
--- src/assets/modules/home.js
+++ src/assets/modules/home.js
@@ -2,6 +2,7 @@
 import bowl1 from '../images/phoBowl.jpg';
 
 export default function home() {
+    
     const el = document.createElement('div');
     el.id = 'home';
 
@@ -10,6 +11,20 @@ export default function home() {
     bowl.id = 'bowl';
     bowl.classList.add('photo');
     el.appendChild(bowl);
+
+    const aboutBox = document.createElement('div');
+    aboutBox.id = 'about-box';
+    const about = document.createElement('div');
+    about.id = 'about';
+    
+    about.textContent = '"Is This Pho Real?" Is what our customers find \
+    themselves asking after each visit. With owners from Saigon, southern style \
+    pho with fresh local ingredients, emphasizing lime and Thai basil are what \
+    we are all about. Our restaurant is 100% vegan.'
+    
+    aboutBox.appendChild(about);
+    el.appendChild(aboutBox);
+
 
     return el;
 }
