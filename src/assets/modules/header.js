264ec302955b26ab7519edf3a2ba46718d4a796b
Pete Fowler <fowler.pete@gmail.com>
2022-06-17 21:19:09 -0600

    Add missing watch script
    It disappeared and index disappeared

--- src/assets/modules/header.js
+++ src/assets/modules/header.js
@@ -7,12 +7,17 @@ const h1 = document.createElement('h1');
 h1.textContent = `Is This Pho Real?`;
 el.appendChild(h1);
 
+const btns = document.createElement('div');
+btns.id = 'btn-group';
+
 const homeBtn = document.createElement('button');
 homeBtn.setAttribute('style', 'id="home" class=".btn"'); // fix id and class
 homeBtn.classList.add('btn');
 homeBtn.id = 'home-btn';
 homeBtn.textContent = 'Home';
-el.appendChild(homeBtn);
+btns.appendChild(homeBtn);
+
+el.appendChild(btns);
 
 return el;
 
