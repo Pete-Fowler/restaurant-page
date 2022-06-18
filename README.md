3d70f2628fa01e7fe159bde12a3b82b62fa30dcd
Pete Fowler <fowler.pete@gmail.com>
2022-06-18 17:34:43 -0600

    Add menu box and two items to home

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# restaurant-page
+Photo by Flo Dahm: https://www.pexels.com/photo/slices-of-breads-1483769/
+Photo by Dương Nhân: https://www.pexels.com/photo/person-trying-to-touch-bowls-1263870/
+Photo by Jer Chung: https://www.pexels.com/photo/bowl-of-fried-food-and-fried-egg-2059151/
--- src/assets/modules/home.js
+++ src/assets/modules/home.js
@@ -1,30 +1,63 @@
 
 import bowl1 from '../images/phoBowl.jpg';
+import apps1 from '../images/apps.jpg';
+import greenApp1 from '../images/greenApp.jpg';
 
 export default function home() {
     
     const el = document.createElement('div');
     el.id = 'home';
 
+    // Bowl hero image
     const bowl = new Image();
     bowl.src = bowl1;
     bowl.id = 'bowl';
-    bowl.classList.add('photo');
+    bowl.setAttribute('alt', 'Picture of bowl of pho');
     el.appendChild(bowl);
 
+    // About paragraph
     const aboutBox = document.createElement('div');
     aboutBox.id = 'about-box';
     const about = document.createElement('div');
     about.id = 'about';
     
     about.textContent = '"Is This Pho Real?" Is what our customers find \
-    themselves asking after each visit. With owners from Saigon, southern style \
-    pho with fresh local ingredients, emphasizing lime and Thai basil are what \
-    we are all about. Our restaurant is 100% vegan.'
+    themselves asking after each visit. Southern style pho with fresh local \
+    ingredients, emphasizing lime and Thai basil are what we are all about. \
+    Our restaurant is 100% vegan.'
     
     aboutBox.appendChild(about);
     el.appendChild(aboutBox);
 
+    // Box for more images of food
+    const menuBox = document.createElement('div');
+    menuBox.id = 'menu-box';
+
+        const box1 = document.createElement('div');
+        box1.classList.add('box');
+            
+            const apps = new Image();
+            apps.src = apps1;
+            apps.id = 'apps';
+            apps.setAttribute('alt', 'Picture of appetizer');
+            apps.classList.add('photo');
+            
+            box1.appendChild(apps);
+            menuBox.appendChild(box1);
+
+        const box2 = document.createElement('div');
+        box2.classList.add('box');
+            
+            const greenApp = new Image();
+            greenApp.src = greenApp1;
+            greenApp.id = 'green-app';
+            greenApp.setAttribute('alt', 'Picture of appetizer');
+            greenApp.classList.add('photo');
+            
+            box2.appendChild(greenApp);
+            menuBox.appendChild(box2);
+
+    el.appendChild(menuBox);
 
     return el;
 }
