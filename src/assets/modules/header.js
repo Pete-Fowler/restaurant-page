6713e571b5b0b3ac8e2f0097fea56de0b3a724a7
Pete Fowler <fowler.pete@gmail.com>
2022-06-18 15:42:09 -0600

    Complete header style

--- src/assets/modules/header.js
+++ src/assets/modules/header.js
@@ -10,11 +10,35 @@ el.appendChild(h1);
 const btns = document.createElement('div');
 btns.id = 'btn-group';
 
-const homeBtn = document.createElement('button');
-homeBtn.classList.add('btn');
-homeBtn.id = 'home-btn';
-homeBtn.textContent = 'Home';
-btns.appendChild(homeBtn);
+// const homeBtn = document.createElement('button');
+// homeBtn.classList.add('btn');
+// homeBtn.id = 'home-btn';
+// homeBtn.textContent = 'Home';
+// btns.appendChild(homeBtn);
+
+// const menuBtn = document.createElement('button');
+// menuBtn.classList.add('btn');
+// menuBtn.id = 'menu-btn';
+// menuBtn.textContent = 'Menu';
+// btns.appendChild(menuBtn);
+
+// const contactBtn = document.createElement('button');
+// contactBtn.classList.add('btn');
+// contactBtn.id = 'contact-btn';
+// contactBtn.textContent = 'Contact';
+// btns.appendChild(contactBtn);
+
+btns.appendChild(makeBtn('Home'));
+btns.appendChild(makeBtn('Menu'));
+btns.appendChild(makeBtn('Contact'));
+
+function makeBtn(name) {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.id = `${name.toLowerCase()}-btn`;
+    btn.textContent = `${name}`;
+    return btn;
+}
 
 el.appendChild(btns);
 
