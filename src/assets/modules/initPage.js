8816d6244d2a6027d3dd9ef7d4ea381e700dcf82
Pete Fowler <fowler.pete@gmail.com>
2022-06-18 20:39:48 -0600

    Add home button click listener

--- src/assets/modules/initPage.js
+++ src/assets/modules/initPage.js
@@ -2,9 +2,10 @@ import header from './header';
 import home from './home';
 
 
-export default function loadPage() {
+export default function initPage() {
 
 const content = document.querySelector('#content');
+content.textContent = '';
 
 content.appendChild(header());
 content.appendChild(home());
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import loadPage from './assets/modules/loadPage';
+import initPage from './assets/modules/initPage';
 import home from './assets/modules/home';
 import contact from './assets/modules/contact';
 import header from './assets/modules/header';
 import menu from './assets/modules/menu';
 import './style.css';
 
-loadPage();
+initPage();
 
+const page = ((e) => {
+    const load = () => {
+        const content = document.querySelector('#content');
+        content.textContent = '';
+
+
+    }
+    return {load};
+})();
+
+const listeners = ((e) => {
+    const home = document.querySelector('#home-btn');
+    home.addEventListener('click', initPage);
+
+    
+
+})();
 
 
 
