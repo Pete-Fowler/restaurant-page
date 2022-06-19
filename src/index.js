ae7a4638b265f0401d14a517468f774be71143f0
Pete Fowler <fowler.pete@gmail.com>
2022-06-18 18:11:45 -0600

    Prepare for page switching logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import loadPage from './assets/modules/loadPage';
+import home from './assets/modules/home';
+import contact from './assets/modules/contact';
+import header from './assets/modules/header';
+import menu from './assets/modules/menu';
 import './style.css';
 
 loadPage();
+
+
 
 
 
